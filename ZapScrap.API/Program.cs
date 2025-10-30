ecb27ceacb2906bbe0fffe565bb776ddcb999130
Gabriel Detofol <detofol29@gmail.com>
2025-10-30 16:40:04 -0300

    test

--- ZapScrap.API/Program.cs
+++ ZapScrap.API/Program.cs
@@ -3,6 +3,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(5);
+    serverOptions.Limits.RequestHeadersTimeout = TimeSpan.FromMinutes(1);
+});
+
+// Configurar tempo de shutdown graceful
+builder.Host.ConfigureServices(services =>
+{
+    services.Configure<HostOptions>(opts =>
+    {
+        opts.ShutdownTimeout = TimeSpan.FromSeconds(30);
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
